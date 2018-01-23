1f3ab43f9a1dbb962e40475211dd9cdd3f50e49b
Aaron Virshup <avirshup@gmail.com>
2018-01-23 10:34:21 -0800

    more packaging

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open('requirements.txt', 'r') as reqfile:
     requirements = [x.strip() for x in reqfile if x.strip()]
 
 setup(
-        name='run-remotely',
+        name='runremotely',
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
-        packages=['run_remotely'],
+        packages=['runremotely'],
         license='Apache 2.0',
         author='Aaron Virshup',
         author_email='avirshup@gmail.com',
         description='Porcelain CLI for syncing data and running commands remotely',
-        url="https://github.com/avirshup/run-remotely",
+        url="https://github.com/avirshup/runremotely",
         entry_points={
                   'console_scripts': [
-                      'remote = run_remotely.__main__:main'
+                      'remote = runremotely.__main__:main'
                   ]
         },
         install_requires=requirements,
